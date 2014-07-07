edd39eda99671928f5066331d7ae0a3a87dc415d
Ian M <imorenodev@gmail.com>
2014-07-07 09:05:58 -0400

    updated app_controller, sessions_controller, app.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user #checks to see who exactly is signed in #can be used in any controller but not views
+    if cookies[:user_id]
+      @user ||= User.find_by(id: cookies.signed[:user_id]) # ||= means @user = @user OR @user = User.find(cookies.signed[:user_id])
+    end
+  end
+  helper_method :current_user #by making current_user a helper method it can be used in any view or controller
+
+  def signed_in? #checks to see if anyone is signed in
+    current_user
+  end
+  helper_method :signed_in?
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,9 @@ class SessionsController < ApplicationController
     cookies.signed[:user_id] = user.id #could user user.email or whatever 
     redirect_to galleries_path
   end
+
+  def destroy
+    cookies.delete(:user_id)
+    redirect_to galleries_path
+  end
 end
